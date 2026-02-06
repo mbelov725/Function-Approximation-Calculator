a2fe09c4da5ab4a0e6622c33cf9fd4618cfa6f5a
mbelov725 <mark.belov725@gmail.com>
2026-02-05 20:47:24 -0500

    Changed max degree to 7 and modified desmos formatting

--- src/desmos_formating.py
+++ src/desmos_formating.py
@@ -2,6 +2,20 @@ def to_desmos_format(expression, degree = "", letter = ""):
     s = str(expression)
     s = s.replace("**", "^")
     s = s.replace("*", "")
+    s = s.replace("(x)", "{x}")
+    s = s.replace("sin", "\\sin")
+    s = s.replace("cos", "\\cos")
+    s = s.replace("tan", "\\tan")
+    s = s.replace("csc", "\\csc")
+    s = s.replace("sec", "\\sec")
+    s = s.replace("cot", "\\cot")
+    s = s.replace("asin", "\\arcsin")
+    s = s.replace("acos", "\\arccos")
+    s = s.replace("atan", "\\arctan")
+    s = s.replace("acsc", "\\arccsc")
+    s = s.replace("asec", "\\arcsec")
+    s = s.replace("acot", "\\arccot")
+    s = s.replace("log", "\\ln")
 
     if degree == "":
         function_name = f"{letter}{degree}"
